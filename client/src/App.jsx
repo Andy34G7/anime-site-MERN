68089d6e7edbdc7babd2a660466a172e054446a3
aivillio <aivillio34@gmail.com>
2025-11-15 06:46:21 +0000

    fixed the contact pg prob

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,7 +5,11 @@ import NavBar from './components/NavBar'
 export default function App() {
   return (
     <>
-      <NavBar />
+      <div className="nav-fullwidth">
+        <NavBar />
+      </div>
+
+      {/* Routed pages render here. Pages control their own full-bleed or centered layout. */}
       <Outlet />
     </>
   )
--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -6,24 +6,25 @@ export default function Navbar() {
     <nav className="nav">
       <div className="nav-blur" />
 
-      <div className="nav-left">
-        <h1 className="logo">ANIME BLOOM</h1>
+      <div className="nav-inner">
+        <div className="nav-left">
+          <h1 className="logo">ANIMEBLOOM</h1>
 
-        <ul className="menu">
-          <li><Link to="/login">Log In</Link></li>
-          <li><Link to="/community">Community</Link></li>
-          <li><Link to="/news">News</Link></li>
-          <li><Link to="/profile/username">Profile</Link></li>
-          <li><Link to="/contact">Contact Us</Link></li>
-        </ul>
+          <ul className="menu">
+            <li><Link to="/login">Log In</Link></li>
+            <li><Link to="/community">Community</Link></li>
+            <li><Link to="/news">News</Link></li>
+            <li><Link to="/profile/username">Profile</Link></li>
+            <li><Link to="/contact">Contact Us</Link></li>
+          </ul>
+        </div>
 
-      </div>
-
-      <div className="nav-right">
-        <div className="search-box">
-          <span className="icon">☰</span>
-          <input type="text" placeholder="Search" />
-          <span className="icon">🔍</span>
+        <div className="nav-right">
+          <div className="search-box">
+            <span className="icon">☰</span>
+            <input type="text" placeholder="Search" />
+            <span className="icon">🔍</span>
+          </div>
         </div>
       </div>
     </nav>
